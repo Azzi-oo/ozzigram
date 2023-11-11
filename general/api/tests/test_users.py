7fe838a50b1ac35dc495bde9c255d019ed472eff
Azat Yanberdin <azatdjon@yandex.ru>
2023-11-11 07:55:42 +0500

    add new tests

--- general/api/tests/test_users.py
+++ general/api/tests/test_users.py
@@ -1,7 +1,9 @@
 from rest_framework.test import APITestCase
 from general.factories import UserFactory
+from django.contrib.auth.hashers import check_password
 from rest_framework import status
 import json
+from general.models import User
 
 
 class UserTestCase(APITestCase):
@@ -33,3 +35,61 @@ class UserTestCase(APITestCase):
             "is_friend": False,
         }
         self.assertDictEqual(response.data["results"][0], expected_data)
+
+    def test_user_list_is_friend_field(self):
+        users = UserFactory.create_batch(5)
+
+        self.user.friends.add(users[-1])
+        self.user.save()
+
+        with self.assertNumQueries(3):
+            response = self.client.get(path=self.url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["results"]), 6)
+
+        self.assertTrue(response.data["results"][0]["is_friend"])
+
+        for user_data in response.data["results"][1::]:
+            self.assertFalse(user_data["is_friend"])
+
+    def test_correct_registration(self):
+        self.client.logout()
+
+        data = {
+            "username": "test_user_1",
+            "password": "12345",
+            "email": "test_user_1@gmail.com",
+            "first_name": "Aza",
+            "last_name": "Yanb",
+        }
+        response = self.client.post(path=self.url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        created_user = User.objects.last()
+        self.assertTrue(check_password(data["password"], created_user.password))
+        data.pop("password")
+
+        user_data = {
+            "username": created_user.username,
+            "email": created_user.email,
+            "first_name": created_user.first_name,
+            "last_name": created_user.last_name,
+        }
+        self.assertDictEqual(data, user_data)
+
+    def test_try_to_pass_existing_username(self):
+        self.client.logout()
+
+        data = {
+            "username": self.user.username,
+            "password": "12345",
+            "email": "test_user_1@gmail.com",
+            "first_name": "Aza",
+            "last_name": "Yanb",
+        }
+
+        response = self.client.post(path=self.url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.all().count(), 1)
+
+    
