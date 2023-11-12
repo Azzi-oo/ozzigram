b106a1ee5f0170fbe05e12deca73236992b4f289
Azat Yanberdin <azatdjon@yandex.ru>
2023-11-12 07:32:01 +0500

    add new test for reaction

--- general/api/tests/test_posts.py
+++ general/api/tests/test_posts.py
@@ -1,8 +1,8 @@
 from rest_framework.test import APITestCase
-from general.factories import UserFactory, PostFactory
+from general.factories import UserFactory, PostFactory, ReactionFactory
 from django.contrib.auth.hashers import check_password
 from rest_framework import status
-from general.models import Post
+from general.models import Post, Reaction
 
 
 class PostTestCase(APITestCase):
@@ -73,3 +73,41 @@ class PostTestCase(APITestCase):
             "created_at": post.created_at.strftime("%Y-%m-%dT%H:%M:%S"),
         }
         self.assertDictEqual(expected_data, response.data["results"][0])
+
+    def test_retrieve_structure(self):
+        post = PostFactory()
+        author = post.author
+        reaction = ReactionFactory(
+            author=self.user,
+            post=post,
+            value=Reaction.Values.HEART,
+        )
+        response = self.client.get(
+            path=f"{self.url}{post.pk}/",
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        expected_data = {
+            "id": post.pk,
+            "author": {
+                "id": author.pk,
+                "first_name": author.first_name,
+                "last_name": author.last_name,
+            },
+            "title": post.title,
+            "body": post.body,
+            "my_reaction": reaction.value,
+            "created_at": post.created_at.strftime("%Y-%m-%dT%H:%M:%S"),
+        }
+        self.assertDictEqual(expected_data, response.data)
+
+    def test_retrieve_structure_without_own_reaction(self):
+        post = PostFactory()
+
+        response = self.client.get(
+            path=f"{self.url}{post.pk}/",
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["my_reaction"], "")
